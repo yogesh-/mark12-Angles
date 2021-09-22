decb1c2d7a389e21510a2eceea4f4c95e2735854
Yogesh Gupta <yogesh.gupta38@gmail.com>
2021-09-23 02:33:39 +0530

    .

---
+++ quiz.js
@@ -0,0 +1,15 @@
+const angles = document.querySelectorAll(".angle-input");
+const checkResult = document.querySelector("#check");
+const result = document.querySelector("#output");
+
+
+checkResult.addEventListener("click",isTriangle);
+
+function isTriangle(){
+    console.log("yolo");
+    
+}
+
+function sumOfAngles(angle1,angle2,angle3){
+     const sum
+}
